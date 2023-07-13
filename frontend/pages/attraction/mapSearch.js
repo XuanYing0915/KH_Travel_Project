ec7428a4de9a86bf1fb11953422fa42507498fd3
XuanYing <xuanyinglife@gmail.com>
2023-07-13 08:38:54 +0800

    0712

---
+++ frontend/pages/attraction/mapSearch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// 引入標題元件
+import Title from '@/components/title'
+
+export default function MapSearch() {
+  return (
+    <>
+      <Title title="標題名稱很多字" />
+    </>
+  )
+}

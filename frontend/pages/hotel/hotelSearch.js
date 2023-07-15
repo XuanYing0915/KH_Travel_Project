2247576366ad1e8b050a79b2caf808595d8e61c2
elbert <sky25247079@hotmail.com>
2023-07-15 17:53:12 +0800

    0715 搜尋欄初步刻板

---
+++ frontend/pages/hotel/hotelSearch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '@/components/search'
+import Title from '@/components/title'
+
+const hotelSearch = () => {
+  return (
+    <div>
+      <Title title="標題" />
+      <Search />
+    </div>
+  )
+}
+
+export default hotelSearch

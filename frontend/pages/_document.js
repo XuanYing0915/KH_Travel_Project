cf8287517bf4fd19814e7a1a28272ad5de90c334
Rynic <wendy9245@gmail.com>
2023-07-18 16:34:55 +0800

    1

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,9 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" className="h-100">
-      <Head />
-      <body className="d-flex flex-column h-100">
+    <Html lang="zh-TW" className="h-150">
+      <Head >123</Head>
+      <body>
+      {/* <body className="d-flex flex-column h-100"> */}
         <Main />
         <NextScript />
       </body>

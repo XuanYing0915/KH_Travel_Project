d2f04f98513d3b4640b61229a6d5d99004ded0d9
XuanYing <xuanyinglife@gmail.com>
2023-07-21 04:10:38 +0800

    0720展示分頁刻板完成

--- frontend/pages/attraction/itinerary.js
+++ frontend/pages/attraction/itinerary.js
@@ -86,9 +86,18 @@ export default function Itinerary({ search, setInput }) {
         {/* 景點詳細頁 */}
         <div className="col-2 i-bg row d-flex flex-column">
           <div className="i-d-content flex-fill">
-            {/* 標題 */}
-            <div className="col i-d-title">標題</div>
-            {/* 標題結束 */}
+            {/* 景點名稱+關閉按鈕 */}
+            <div className="row justify-content-between name-close">
+              {/* 景點名稱 */}
+              <div className="col">景點名稱</div>
+              {/* 關閉按鈕 */}
+              <button
+                type="button"
+                class="btn-close btn-close-white"
+                aria-label="Close"
+              ></button>
+            </div>
+            {/* 景點名稱+關閉按鈕結束 */}
             {/* 圖片 */}
             <div className="col">
               <img src="/images/attraction/草神.jpg" />
@@ -98,16 +107,19 @@ export default function Itinerary({ search, setInput }) {
             <div className="col d-content row d-flex flex-column">
               <div className="col ">
                 <i className="bi bi-alarm-fill"></i>
+                {/* TODO 資料庫增加停留欄位 */}
                 建議停留時間 : 1小時30分
               </div>
               <div className="col ">
                 <i class="bi bi-geo-alt-fill"></i>
                 高雄市鹽埕區真愛路1號
               </div>
+              {/* 營業時間 */}
               <div className="col ">
                 <i class="bi bi-info-circle-fill"></i>營業時間
               </div>
               <div className="time d-flex align flex-column">
+                {/* TODO 用公休判斷營業日期 */}
                 <div>星期二 10:00 – 22:00</div>
                 <div>星期二 10:00 – 22:00</div>
                 <div>星期二 10:00 – 22:00</div>
@@ -116,13 +128,23 @@ export default function Itinerary({ search, setInput }) {
                 <div>星期二 10:00 – 22:00</div>
                 <div>星期二 10:00 – 22:00</div>
               </div>
+              {/* 營業時間結束 */}
               <hr />
-              <div className=""></div>
+              {/* 簡介 */}
+              <div className="summary">
+                走進絢爛的星空隧道，體驗10種不同的主題光影動畫，來一趟奇幻空間的打卡之旅。
+              </div>
             </div>
-
             {/* 內容結束 */}
             {/* 按鈕 */}
-            <div className="col">按鈕</div>
+            <div className="col  row justify-content-evenly align-items-end">
+              <button className="col-4 add-i-btn rounded-pill ">
+                加入行程
+              </button>
+              <button className="col-4 add-f-btn rounded-pill ">
+                加入收藏
+              </button>
+            </div>
             {/* 按鈕結束 */}
           </div>
         </div>

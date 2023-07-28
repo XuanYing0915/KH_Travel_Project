2c865de2af845c6ac9ee5abd9d37646617824c7f
elbert <sky25247079@hotmail.com>
2023-07-28 15:44:41 +0800

    0728合併前上傳

--- frontend/pages/hotel/hoteldetail.js
+++ frontend/pages/hotel/hoteldetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '@/components/title'
-// import Card2 from '@/components/common-card2/common-card2'
+import Card2 from '@/components/common-card2/common-card2'
 import Weather from '@/components/hotel/weather'
 
 export default function hotelSearch() {
@@ -51,18 +51,75 @@ export default function hotelSearch() {
         </section>
         <Title title="周邊景點" style="title_box_dark" />
         <div className="d-flex">
-          {/* <Card2 />
-          <Card2 />
-          <Card2 />
-          <Card2 /> */}
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
         </div>
         <Title title="周邊美食" style="title_box_dark" />
         <div className="d-flex">
-          {/* <Card2 />
-          <Card2 />
-          <Card2 />
-          <Card2 /> */}
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
+          <Card2
+            id={1}
+            img_src="洲際.jpg"
+            name="洲際飯店"
+            like={true}
+            towheresrc="#"
+            imgrouter="hotel"
+          />
         </div>
+        <div style={{ margin: '50px' }}></div>
       </div>
     </>
   )

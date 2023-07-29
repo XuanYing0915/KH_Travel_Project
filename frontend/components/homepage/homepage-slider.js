b99c17994110d4066473acf60ac0591fe87ef1ff
Rynic <wendy9245@gmail.com>
2023-07-29 14:00:27 +0800

    0729

--- frontend/components/homepage/homepage-slider.js
+++ frontend/components/homepage/homepage-slider.js
@@ -10,14 +10,40 @@ export default function HomepageSlider() {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    target : {
+      displayHeight: 400,
+      displayWidth: 300
+    }
   }
+  
+  
+  // getImageOriginalResolution = (base64) => {
+  //     const image = new Image();
+  //     image.onload = () => {
+  //       this.setImageResolution(image.width, image.height)
+  //       this.setState((ps) => ({...ps, imgRatio: image.width / image.height}))
+  //     };
+  //     image.src = base64;
+  //   };
+  //   setImageResolution = (imageWidth, imageHeight) => {
+  //     const {target} = this.settings;
+  //     let w = (imageWidth / imageHeight) * target.displayHeight;
+  //     let h = (imageHeight / imageWidth) * target.displayWidth;
+  //     imageWidth / imageHeight > target.displayWidth / target.displayHeight ? h = settings.displayHeight : w = settings.displayWidth;
+  //     const marginLeft = (target.displayWidth / 2) - (w * 0.5);
+  //     const marginTop = (target.displayHeight / 2) - (h * 0.5);
+  //     this.setState((ps) => ({...ps, imageHeight: h, imageWidth: w, imgMarginLeft: marginLeft, imgMarginTop: marginTop}))
+  //   }
+    
+    // <img style={{width: imageWidth + 'px', height: imageHeight + 'px', marginLeft: imgMarginLeft + 'px', marginTop: imgMarginTop + 'px'}}/>
   return (
-    <div>
+    <div style={{maxHeight:'100%',maxWidth:'100%'}}>
         
         <Slider {...settings}>
-          <div className='slide-pic' style={{height:'550px'}}>
-            <img src="/images/index/高流.jpg" />
+          <div className='slide-pic' >
+            <img src="/images/index/高流.jpg" style={{maxWidth:'100%', objectFit:'contain'}}/>
           </div>
           <div className='slide-pic'>
             <img src="images\index\高捷美麗島站.jpg" />
@@ -27,5 +53,6 @@ export default function HomepageSlider() {
           </div>
         </Slider>
     </div>
+    
   )
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,17 +4,14 @@ import Link from 'next/link'
 import Image from 'next/image'
 import PlaceholderText from '@/components/common/placeholder-text'
 import background from '@/public/images/index/高流.jpg'
-import { FaFacebook } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
 // import Slider from "react-slick";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-import HomepageSlider from '@/components/homepage/homepage-slider';
+import HomepageSlider from '@/components/homepage/homepage-slider'
 import Card2 from '@/components/common-card2/common-card2'
-
-
-
 
 export default function Home() {
   const settings = {
@@ -23,25 +20,51 @@ export default function Home() {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   }
   return (
     <>
       <main style={{ marginBlock: '90px' }}>
         {/* 1. 輪播圖 */}
-        <div className='d-flex flex-row ' style={{ height: '-webkit-calc(100vh - 120px)', marginBottom: '50px' }} id='homepage-1'>
-          <div className='d-flex flex-column align-items-center justify-content-end' style={{ width: '160px', paddingBottom: '20vh' }}>
-            <a href="#"><FaFacebook className='media-icon' /></a>
-            <a href="#"><FaTwitter className='media-icon' /></a>
-            <a href="#"><FaInstagram className='media-icon' /></a>
+        <div
+          className="d-flex flex-row "
+          style={{
+            height: '-webkit-calc(100vh - 120px)',
+            marginBottom: '50px',
+          }}
+          id="homepage-1"
+        >
+          {/*1-1.icon列  */}
+          <div
+            className="d-flex flex-column align-items-center justify-content-end"
+            style={{ width: '160px', paddingBottom: '20vh' }}
+          >
+            <a href="#">
+              <FaFacebook className="media-icon" />
+            </a>
+            <a href="#">
+              <FaTwitter className="media-icon" />
+            </a>
+            <a href="#">
+              <FaInstagram className="media-icon" />
+            </a>
           </div>
-          <div className='homepage-pic'
+          {/* <div className='homepage-pic'
             style={{ backgroundImage: `url(${background.src})` }} >
             <span className='homepage-text' id='homepage-text1'>在高雄</span>
             <span className='homepage-text' id='homepage-text2'>盡情探索驚喜與美景</span>
+          </div> */}
+          {/* 1-2. 輪播圖 */}
+          <div className="slider-container">
+            <HomepageSlider />
+            <span className="homepage-text" id="homepage-text1">
+              在高雄
+            </span>
+            <span className="homepage-text" id="homepage-text2">
+              盡情探索驚喜與美景
+            </span>
           </div>
-          <div style={{ width: '160px' }}>
-          </div>
+          <div style={{ width: '160px' }}></div>
         </div>
 
         {/* <div className='container'>
@@ -50,8 +73,15 @@ export default function Home() {
         {/* <HomepageSlider /> */}
 
         {/* 2.熱門景點 */}
-        <div style={{ backgroundColor: '#0D5654', height: '-webkit-calc(100vh - 120px)' }} id='homepage-2'>
-          <div className='d-flex flex-row'>
+        <div
+          style={{
+            backgroundColor: '#0D5654',
+            height: '-webkit-calc(100vh - 120px)',
+            marginTop: '50px',
+          }}
+          id="homepage-2"
+        >
+          <div className="d-flex flex-row">
             <Card2
               id={1}
               img_src="溫迪.png"
@@ -93,17 +123,11 @@ export default function Home() {
               imgrouter="attraction"
             />
           </div>
-
-
-
-
         </div>
 
         {/* 3.天氣API */}
 
         {/* 4.卡片to各頁面 */}
-
-
 
         {/* <Slider {...settings}>
           <div className='slide-pic' style={{height:'550px'}}>
@@ -116,58 +140,67 @@ export default function Home() {
             <img src="images/index/河景.jpg" />
           </div>
         </Slider> */}
-
-
       </main>
 
       <style global jsx>
         {`
-         .homepage-pic{
-          position:relative;
-          width: 100%;
-          max-height: 75vh;
-          overflow: hidden;
-          
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-position: 35% 40%;
-         }
-         .homepage-text{
-          background:white;
-          padding-inline:20px;
-          font-size:24px;
-          font-weight:700;
-         }
-         #homepage-text1{
-          position:absolute;
-          bottom:35%;
-          right:80px;
-         }
-         #homepage-text2{
-          position:absolute;
-          bottom:calc(35% - 70px);
-          right:80px;
-         }
-         .slick-slide {
-          height: auto; 
-        }
-        .slick-track {
-          display: flex;
-          flex-direction: row;
-          flex-wrap: nowrap;
-          align-items: stretch;
-        }
+          .homepage-pic {
+            position: relative;
+            width: 100%;
+            max-height: 75vh;
+            overflow: hidden;
 
-        .media-icon{
-          font-size:30px;
-          color:#5F5F5F;
-          margin-top:12px;
-        }
-        .media-icon:hover{
-          
-          color:#B1B1B1;
-          
-        }
+            {/* background-repeat: no-repeat;
+          background-size: cover;
+          background-position: 35% 40%; */
+            }
+          }
+
+          .slider-container {
+            position: relative;
+            max-width: 100vw;
+            width: 100%;
+            overflow: hidden;
+          }
+          .slide-pic{
+            object-fit: contain;
+            object-position: 35% 40%;
+          }
+
+          .homepage-text {
+            background: white;
+            padding-inline: 20px;
+            font-size: 24px;
+            font-weight: 700;
+          }
+          #homepage-text1 {
+            position: absolute;
+            bottom: 35%;
+            right: 80px;
+          }
+          #homepage-text2 {
+            position: absolute;
+            bottom: calc(35% - 70px);
+            right: 80px;
+          }
+          .slick-slide {
+            height: auto;
+          }
+          .slick-track {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            align-items: stretch;
+          }
+
+          .media-icon {
+            font-size: 30px;
+            color: #5f5f5f;
+            margin-top: 12px;
+          }
+          .media-icon:hover {
+            color: #b1b1b1;
+          }
 
           .card-cover {
             background-repeat: no-repeat;

df3c9b0766c316c47be103aed182740d98412db6
utlxj123 <132909849+utlxj123@users.noreply.github.com>
2023-07-29 22:19:48 +0800

    7/29 (家) 建立地圖查詢標題一半

---
+++ frontend/components/food/map-query-title.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import styles from './map-query-title.module.scss'
+
+import React from 'react'
+
+export default function MapQueryTitle() {
+  return (
+    <>
+      <div className={styles['map-query-title']}>mapQuery</div>
+    </>
+  )
+}
--- frontend/pages/food/store-queryt.js
+++ frontend/pages/food/store-queryt.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Introductioncard from '@/components/food/Introduction-card'
+import Introductioncard from '@/components/food/Introduction-card'
 
+import styles from '@/styles/food.module.scss'
 export default function index() {
   const card = []
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1; i++) {
     card.push(<Introductioncard />)
   }
   return (
     <>
-      <div className="queryt-body">
+      <div className={styles['queryt-body']}>
         <div className="img-dark-bg"></div>
         <div>123</div>
         <div className="row d-flex justify-content-center">{card}</div>

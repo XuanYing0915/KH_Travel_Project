365e1851b6b586d05e41f7139dd8bf3ad43a2504
ben <utlxj123@gmail.com>
2023-07-30 16:45:21 +0800

    7/30(校) 地圖定位改一半

---
+++ frontend/components/food/map-query.js
@@ -0,0 +1,99 @@
+import styles from './map-query.module.scss'
+
+import React from 'react'
+
+export default function MapQueryTitle() {
+  return (
+    <>
+      <div className={styles['map-query']}>
+        {/* 高雄市地圖 */}
+        <div className={styles['map']}>
+          <img src="/images/food/高雄市區地圖.svg" />
+        </div>
+
+        {/* 箭頭區域標題 */}
+        <div>
+          <div className={styles['container-1']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>左營區</h2>
+              <p>蓮池潭商圈、瑞豐夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-2']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>鼓山區</h2>
+              <p>風華再現商圈</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-3']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>鹽埕區 前金區 新興區</h2>
+              <p>鹽埕崛江商圈、新崛江商圈、鹽埕埔夜市、六合夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-4']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>苓雅區</h2>
+              <p>苓雅自強夜市、光華夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-5']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>旗津區</h2>
+              <p>旗津老街 </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-6']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>前鎮區</h2>
+              <p>凱旋夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-7']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>鳳山區</h2>
+              <p>鳳山青年夜市、鳳山自強夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-8']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>鳥松區</h2>
+              <p>鳥松夜市</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles['container-9']}>
+            <img src="/images/food/箭頭標示.svg" />
+            <div className={styles['text-container']}>
+              <h2>三民區</h2>
+              <p>後驛商圈、大連商圈、吉林夜市</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- frontend/pages/food/store-queryt.js
+++ frontend/pages/food/store-queryt.js
@@ -1,18 +1,17 @@
 import React from 'react'
-import Introductioncard from '@/components/food/Introduction-card'
-import Introductioncard from '@/components/food/Introduction-card'
-
+import IntroductionCard from '@/components/food/Introduction-card'
+import MapQuery from '@/components/food/map-query'
 import styles from '@/styles/food.module.scss'
 export default function index() {
   const card = []
   for (let i = 0; i < 1; i++) {
-    card.push(<Introductioncard />)
+    card.push(<IntroductionCard />)
   }
   return (
     <>
-      <div className={styles['queryt-body']}>
+      <div className={styles['query-body']}>
         <div className="img-dark-bg"></div>
-        <div>123</div>
+        <MapQuery />
         <div className="row d-flex justify-content-center">{card}</div>
       </div>
     </>

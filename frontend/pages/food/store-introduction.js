ac8425e275c66f1e797e510718f09fba58e9fc23
utlxj123 <132909849+utlxj123@users.noreply.github.com>
2023-08-02 16:30:25 +0800

    8/2(校)介紹排版上部分完成

--- frontend/pages/food/store-introduction.js
+++ frontend/pages/food/store-introduction.js
@@ -7,13 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
 
-// 測試卡片
-export default function Index() {
-  const card = []
-  for (let i = 0; i < 10; i++) {
-    card.push(<IntroductionCard />)
-  }
-
+export default function Index({ img_src = '2017-07-02.jpg' }) {
   // 收藏愛心
   const [isFavorited, setFavorited] = useState(false)
   const favoriteIcon = isFavorited ? fasHeart : farHeart
@@ -22,8 +16,17 @@ export default function Index() {
   }
 
   // 評分
-  const JSON = {
-    rating: 3.9,
+  const rating = {
+    rating: 4.5,
+  }
+
+  // 介紹圖片
+  const img = `/images/food/${img_src}`
+
+  // 測試卡片
+  const card = []
+  for (let i = 0; i < 10; i++) {
+    card.push(<IntroductionCard />)
   }
 
   return (
@@ -33,31 +36,50 @@ export default function Index() {
         {/* 頁首空間 */}
         <div className={styles['head-space']}></div>
 
-        {/* 商家名、介紹文、評分、收藏愛心 */}
-        <h1>貳樓</h1>
-        <h2>Second Floor Cafe</h2>
-        <div>
-          <StarRating rating={JSON.rating} />
-          <span>{JSON.rating}</span>
+        {/* 商家名、評分、星星、收藏愛心 */}
+        <div className={styles['title-love-img']}>
+          <div className={styles['title-with-love']}>
+            <div>
+              {/* 商家名 */}
+              <h1>貳樓</h1>
+              <h2 className={styles['english-title']}>Second Floor Cafe</h2>
+              <div className={styles['rating-star']}>
+                {/* 評分 */}
+                <div>{rating.rating}</div>
+                <StarRating rating={rating.rating} />
+              </div>
+            </div>
+            {/* 收藏愛心 */}
+            <div onClick={toggleFavorite}>
+              <FontAwesomeIcon
+                icon={favoriteIcon}
+                className={styles['favoriteIcon']}
+              />
+            </div>
+          </div>
+          {/* 介紹圖片 */}
+          <img src={img} alt="Food Introduction" />
         </div>
-
-        {/* 收藏愛心 */}
-        <div onClick={toggleFavorite}>
-          <FontAwesomeIcon
-            icon={favoriteIcon}
-            className={styles['favoriteIcon']}
-          />
+        {/* 介紹文 */}
+        <div className={styles['introductory-text']}>
+          <h2>午後的緣起</h2>
+          <br />
+          <p>
+            隱密的巷弄間靜靜地倚著大樹的獨棟建築，這是我們的起點。
+            取名為「貳樓」是因為大樓林立的都市裡二樓不會是個商辦空間，也不會是個營業場所；二樓是一間間我們在精神上依靠的住家，同時一份份的溫暖也從二樓開始發生。貳樓就想帶給顧客最純粹的『家的感覺』，裡面有我們希望營造的溫暖自在氛圍以及用心、創意的新美式料理。在這裡，用料理分享愛、用愛創造人情味
+          </p>
+          <p>
+            Sharing love , sharing food. That's all about Second Floor Cafe.
+          </p>
         </div>
-        <h2>午後的緣起</h2>
-        <p>
-          隱密的巷弄間靜靜地倚著大樹的獨棟建築，這是我們的起點。
-          取名為「貳樓」是因為大樓林立的都市裡二樓不會是個商辦空間，也不會是個營業場所；二樓是一間間我們在精神上依靠的住家，同時一份份的溫暖也從二樓開始發生。貳樓就想帶給顧客最純粹的『家的感覺』，裡面有我們希望營造的溫暖自在氛圍以及用心、創意的新美式料理。在這裡，用料理分享愛、用愛創造人情味
-        </p>
-        <p>Sharing love , sharing food. That's all about Second Floor Cafe.</p>
-
+        {/* 介紹圖片 */}
+        <div className={styles['images-container']}>
+          <img src={img} alt="Food Introduction" />
+          <img src={img} alt="Food Introduction" />
+          <img src={img} alt="Food Introduction" />
+        </div>
         {/* 測試卡片 */}
         <div className="row d-flex justify-content-center">{card}</div>
-
         {/* 頁尾空間 */}
         <div className={styles['footer-space']}></div>
       </div>

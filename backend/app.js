398e62903bb3b9ef4ef0012f3b9cbefdc9622e59
XuanYing0915 <xuanyinglife@gmail.com>
2023-08-03 11:12:54 +0800

    app修完bug

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ var app = express();
 const cors = require("cors");
 var flash = require("connect-flash");
 var validator = require("express-validator");
-const cors = require("cors");
 var flash = require("connect-flash");
 var validator = require("express-validator");
 // view engine setup
@@ -30,7 +29,6 @@ app.use(express.static("public"));
 var session = require("express-session");
 app.use(express.static("public"));
 app.use(
-  session({ secret: "mysupersecret", resave: true, saveUninitialized: true })
   session({ secret: "mysupersecret", resave: true, saveUninitialized: true })
 );
 app.use(flash());
@@ -58,9 +56,6 @@ const AIRouter = require("./routes/attraction/itinerary");
 const ticketRouter = require("./routes/ticket/ticketAllData");
 // 設定跨域 只接受3000port
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
   cors({
     origin: "http://localhost:3000",
   })
@@ -121,14 +116,7 @@ app.use(function (req, res, next) {
 // error handlers
 // development error handler
 // will print stacktrace
-if (app.get("env") === "development") {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render("error", {
-      message: err.message,
-      error: err,
-    });
-  });
+
 if (app.get("env") === "development") {
   app.use(function (err, req, res, next) {
     res.status(err.status || 500);
@@ -148,20 +136,6 @@ app.use(function (err, req, res, next) {
   res.render("error", {
     message: err.message,
     error: err,
-  });
-});
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {},
-  });
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {},
   });
 });
 module.exports = app;

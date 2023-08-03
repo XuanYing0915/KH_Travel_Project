987d4b13906ead6009d66873b0c427a8d6c92595
elbert <sky25247079@hotmail.com>
2023-08-03 11:40:57 +0800

    0803APP.js問題合併前上傳

---
+++ backend/routes/hotel/intermediary.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../connections/mysql_config.js");
+
+// 一開始先顯示所有行程
+//TODO: 這裡要改成顯示該地區的景點
+router.route("/").get(async (req, res) => {
+  const sql = `SELECT 
+  intermediary_id,
+  hotel_name,
+  facility_name
+  FROM hotel_intermediary
+  JOIN hotel_kh ON hotel_intermediary.hotel_id = hotel_kh.hotel_id
+  JOIN hotel_facility ON hotel_intermediary.facility_id = hotel_facility.facility_id
+  ORDER BY hotel_intermediary.intermediary_id ASC
+  `;
+  const [datas] = await db.query(sql);
+  res.json(datas);
+});
+
+module.exports = router;

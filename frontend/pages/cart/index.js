6a2bf3f27db6171a7e9a2c8f7c68ea6013d13be2
Rynic <wendy9245@gmail.com>
2023-08-04 09:17:19 +0800

    0804-1

--- frontend/pages/cart/index.js
+++ frontend/pages/cart/index.js
@@ -1,184 +1,35 @@
 import React from 'react'
+import CardList from '@/components/cart/cart-list'
 
 export default function CartIndex() {
   return (
-    <>
-      {/* <div className="row mt-5 mx-5">
-        <div className="col-sm-8 cart-area">
-          <h4 className="mb-3">購物車</h4>
-          <div className="card mb-3 border-0 cart-card">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img
-                  src="./images/cart-1.jpeg"
-                  className="img-fluid rounded-start"
-                  alt="..."
-                />
-              </div>
-              <div className="col-md-9">
-                <div className="card-body">
-                  <h5 className="card-title card-text d-flex justify-content-between align-items-center">
-                    Nike Air Force 1 PLT.AF.ORM <span>$4,000.00</span>
-                  </h5>
-                  <p className="card-text">
-                    Pale Ivory/Light Orewood Brown/白/Summit White
-                  </p>
-
-                  <div className="row g-3 align-items-center">
-                    <div className="col-auto">
-                      <label
-                        htmlFor="inputPassword6"
-                        className="col-form-label"
-                      >
-                        數量:
-                      </label>
-                    </div>
-                    <div className="col-auto">
-                      <select
-                        className="form-select form-select-sm"
-                        aria-label=".form-select-sm example"
-                      >
-                        <option selected>0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div className="iconbar">
-                    <i className="bi bi-suit-heart"></i>
-                    <i className="bi bi-trash3"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="card mb-3 border-0 cart-card">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img
-                  src="./images/cart-1.jpeg"
-                  className="img-fluid rounded-start"
-                  alt="..."
-                />
-              </div>
-              <div className="col-md-9">
-                <div className="card-body">
-                  <h5 className="card-title card-text d-flex justify-content-between align-items-center">
-                    Nike Air Force 1 PLT.AF.ORM <span>$4,000.00</span>
-                  </h5>
-                  <p className="card-text">
-                    Pale Ivory/Light Orewood Brown/白/Summit White
-                  </p>
-
-                  <div className="row g-3 align-items-center">
-                    <div className="col-auto">
-                      <label
-                        htmlFor="inputPassword6"
-                        className="col-form-label"
-                      >
-                        數量:
-                      </label>
-                    </div>
-                    <div className="col-auto">
-                      <select
-                        className="form-select form-select-sm"
-                        aria-label=".form-select-sm example"
-                      >
-                        <option selected>0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div className="iconbar">
-                    <i className="bi bi-suit-heart"></i>
-                    <i className="bi bi-trash3"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="card mb-3 border-0 cart-card">
-            <div className="row g-0">
-              <div className="col-md-3">
-                <img
-                  src="./images/cart-1.jpeg"
-                  className="img-fluid rounded-start"
-                  alt="..."
-                />
-              </div>
-              <div className="col-md-9">
-                <div className="card-body">
-                  <h5 className="card-title card-text d-flex justify-content-between align-items-center">
-                    Nike Air Force 1 PLT.AF.ORM <span>$4,000.00</span>
-                  </h5>
-                  <p className="card-text">
-                    Pale Ivory/Light Orewood Brown/白/Summit White
-                  </p>
-
-                  <div className="row g-3 align-items-center">
-                    <div className="col-auto">
-                      <label
-                        htmlFor="inputPassword6"
-                        className="col-form-label"
-                      >
-                        數量:
-                      </label>
-                    </div>
-                    <div className="col-auto">
-                      <select
-                        className="form-select form-select-sm"
-                        aria-label=".form-select-sm example"
-                      >
-                        <option selected>0</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div className="iconbar">
-                    <i className="bi bi-suit-heart"></i>
-                    <i className="bi bi-trash3"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr />
-        </div>
-        <div className="col-sm-4">
-          <h4 className="mb-3">摘要</h4>
-
-          <p className="card-text d-flex justify-content-between align-items-center">
-            小計 <span>$4,000.00</span>
-          </p>
-          <hr />
-          <p className="card-text d-flex justify-content-between align-items-center">
-            預估運費與手續費 <span>$300.00</span>
-          </p>
-          <hr />
-          <p className="card-text d-flex justify-content-between align-items-center">
-            總計 <span>$4,300.00</span>
-          </p>
-          <hr />
-          <button className="btn btn-primary w-100 mb-3">會員結帳</button>
-          <button className="btn btn-primary w-100 mb-3">訪客結帳</button>
-        </div>
+    <div id="cart-index">
+      {/* 1.購物順序 */}
+      <div className='d-flex justify-content-between border-bottom' id="cart-steps">
+        <p className="now-step">1<span>確認購物車內商品</span></p>
+        <p >2<span>填寫付款與運送資訊</span></p>
+        <p>3<span>完成購買</span></p>
       </div>
-      <div className="row mt-5 mx-5">
-        <div className="col-sm-12 cart-area">
-          <h4 className="mb-3">最愛</h4>
-          <p>想要檢視你的最愛嗎？ 加入我們 或 登入</p>
-        </div>
-      </div> */}
-    </>
+
+      {/* 2.購物車內商品 */}
+      <div className='py-3' id="cart-products">
+        {/* 2-1 選擇商品類型 */}
+        <ul className='d-flex'>
+          <li>美食商品(<span>0</span>)</li>
+          <li className='now-product-type'>票券商品(<span>2</span>)</li>
+          <li>住宿商品(<span>0</span>)</li>
+        </ul>
+
+        {/* 2-2 購物車table */}
+        <CardList />
+      </div>
+      {/* 3.按鈕列 */}
+      <div>
+        <button className='btn ' style={{ backgroundColor: '#7FB8B6', color: '#fff' }}>繼續購物</button>
+        <button className='btn ' style={{ backgroundColor: '#D05B62', color: '#fff' }}>刪除全部商品</button>
+        <button className='btn ' style={{ backgroundColor: '#FFc700', color: '#fff' }}>去買單</button>
+      </div>
+
+    </div>
   )
 }

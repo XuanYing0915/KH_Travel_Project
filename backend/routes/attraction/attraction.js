f4006c4606f95b1bea5de4a4ac057bc19a5d7436
XuanYing0915 <xuanyinglife@gmail.com>
2023-08-04 08:28:49 +0800

    0804早

--- backend/routes/attraction/attraction.js
+++ backend/routes/attraction/attraction.js
@@ -2,41 +2,82 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../connections/mysql_config.js');
 
+// sql語法
+
+// 取得所有景點資料
 router.route('/').get(async (req, res) => {
-	const sql = ``;
+	const sql = `SELECT
+  att.attraction_id,
+  att.attraction_name,
+  att.title,
+  att.fk_area_id,
+  area.area_name,
+  att.address,
+  att.off_day,
+  att.open_time,
+  att.closed_time,
+  att.phone,
+  att.description,
+  att.lat,
+  att.lng,
+  att.zoom,
+  att.traffic,
+  GROUP_CONCAT(DISTINCT tag.tag_name) AS tags,
+  GROUP_CONCAT(DISTINCT img.img_name) AS images
+FROM
+  attraction AS att
+LEFT JOIN
+  area AS area ON att.fk_area_id = area.area_id
+LEFT JOIN
+  attraction_hegtag hegtag ON att.attraction_id = hegtag.fk_attraction_id
+LEFT JOIN
+  attraction_tag_name tag ON hegtag.fk_tag_name_id = tag.tag_name_id
+LEFT JOIN
+  attraction_image img ON att.attraction_id = img.fk_attraction_id
+WHERE attraction_id= 600001
+GROUP BY
+  att.attraction_id;
+`;
 	const [datas] = await db.query(sql);
 	res.json(datas);
 });
 
 // 針對回傳的景點id取得景點資料
-router.route('/:id').get(async (req, res) => {
-    const sql = `SELECT 
-    a.attraction_id,
-    a.attraction_name,
-    a.title,
-    a.fk_area_id,
-    area.area_name,
-    a.address,
-    a.off_day,
-    a.open_time,
-    a.closed_time,
-    a.phone,
-    a.description,
-    a.lat,
-    a.lng,
-    a.zoom,
-    a.traffic,
-    MIN(ai.img_name) AS img_name
+router.route('/:attraction_id').get(async (req, res) => {
+	const sql = `SELECT
+  att.attraction_id,
+  att.attraction_name,
+  att.title,
+  att.fk_area_id,
+  area.area_name,
+  att.address,
+  att.off_day,
+  att.open_time,
+  att.closed_time,
+  att.phone,
+  att.description,
+  att.lat,
+  att.lng,
+  att.zoom,
+  att.traffic,
+  GROUP_CONCAT(DISTINCT tag.tag_name) AS tags,
+  GROUP_CONCAT(DISTINCT img.img_name) AS images
 FROM
-    attraction a
-LEFT JOIN attraction_image ai ON a.attraction_id = ai.fk_attraction_id
-LEFT JOIN area ON a.fk_area_id = area.area_id
-GROUP BY a.attraction_id
-HAVING a.attraction_id = ?`;
-    const [datas] = await db.query(sql, [req.params.id]);
-    res.json(datas);
-    res.send(`id: ${req.params.id}`)
+  attraction AS att
+LEFT JOIN
+  area AS area ON att.fk_area_id = area.area_id
+LEFT JOIN
+  attraction_hegtag hegtag ON att.attraction_id = hegtag.fk_attraction_id
+LEFT JOIN
+  attraction_tag_name tag ON hegtag.fk_tag_name_id = tag.tag_name_id
+LEFT JOIN
+  attraction_image img ON att.attraction_id = img.fk_attraction_id
+WHERE att.attraction_id= 600001
+GROUP BY
+  att.attraction_id
+;`;
+	const attractionId = req.params.attraction_id; // 這裡修正使用 req.params.attraction_id
+	const [datas] = await db.query(sql, [attractionId]);
+	res.json(datas);
 });
 module.exports = router;
-
-

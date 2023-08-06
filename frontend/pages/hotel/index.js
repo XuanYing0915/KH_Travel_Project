74ae50733df36650be72957b3861d5f9bb8c8cbc
elbert <sky25247079@hotmail.com>
2023-08-06 23:07:38 +0800

    0806睡覺前路由測試OK

--- frontend/pages/hotel/index.js
+++ frontend/pages/hotel/index.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import Hotelphoto from '@/components/hotel/hotelphoto'
 import Pscall from '@/components/hotel/pscall'
-
+//0806測試上傳
 
 export default function hotelSearch() {
   return (
     <>
-      <div className='hotelSearchBody'>
+      <div className="hotelSearchBody">
         <div>
           <Hotelphoto />
         </div>
         <div>
           {/* <div className="divsearch"> */}
           <div className="container">
-            <Pscall />     
+            <Pscall />
           </div>
         </div>
       </div>

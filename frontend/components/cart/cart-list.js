7a39229699627724303ff7ebd7e1f059580bc90c
Rynic <wendy9245@gmail.com>
2023-08-08 09:38:55 +0800

    0808-1

--- frontend/components/cart/cart-list.js
+++ frontend/components/cart/cart-list.js
@@ -1,11 +1,75 @@
 import React from 'react'
 
 export default function CardList() {
+  const data = [
+    {
+      "product-type": 2,
+      "id": 1,
+      "picture": "https://via.placeholder.com/120.png",
+      "name": "A門票",
+      "type": "大人",
+      "price": "599",
+      "number": "1",
+      "total": "599"
+    },
+    {
+      "product-type": 2,
+      "id": 2,
+      "picture": "https://via.placeholder.com/120.png",
+      "name": "A門票",
+      "type": "兒童",
+      "price": 529,
+      "number": 2,
+      "total": 1058
+    },
+    {
+      "product-type": 2,
+      "id": 3,
+      "picture": "https://via.placeholder.com/120.png",
+      "name": "A門票",
+      "type": "兒童",
+      "price": 529,
+      "number": 2,
+      "total": 1058
+    },]
   return (
-    <>
-        {/* <table>
-            <tr></tr>
-        </table> */}
-    </>
+    <div >
+
+      <table className="col-12 mb-5" id="cart-list">
+        <thead >
+          <tr>
+            <th className='col-5'>品名</th>
+            <th className='col-3'>規格</th>
+            <th>單價</th>
+            <th>數量</th>
+            <th>小計</th>
+            <th>刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((v, i) => {
+            return (
+              <>
+                <tr key={v.id}>
+                  <td>
+                    <img src={v.picture}></img>
+                    <a className='ps-4 fw-bolder text-decoration-underline' href=''>{v.name}</a>
+                  </td>
+                  <td>{v.type}</td>
+                  <td>{v.price}</td>
+                  <td>{v.number}</td>
+                  <td>{v.price}*{v.number}</td>
+                  <td></td>
+                </tr>
+              </>
+            )
+          })}
+
+        </tbody>
+
+
+      </table>
+      <p>共3項商品</p>
+    </div>
   )
 }
--- frontend/pages/cart/index.js
+++ frontend/pages/cart/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 import CardList from '@/components/cart/cart-list'
 
 export default function CartIndex() {
+  //現在購物欄位
+  const [type, setType] = useState(1);
+  let productType = 1;
+
   return (
     <div id="cart-index">
       {/* 1.購物順序 */}
@@ -12,19 +16,28 @@ export default function CartIndex() {
       </div>
 
       {/* 2.購物車內商品 */}
-      <div className='py-3' id="cart-products">
+      <div className='py-4' id="cart-products">
         {/* 2-1 選擇商品類型 */}
         <ul className='d-flex'>
-          <li>美食商品(<span>0</span>)</li>
-          <li className='now-product-type'>票券商品(<span>2</span>)</li>
-          <li>住宿商品(<span>0</span>)</li>
+          <li
+            onClick={() => {
+              productType = 1
+            }}>美食商品(<span>0</span>)</li>
+          <li
+            onClick={() => {
+              productType = 2
+            }} className={productType = 2 ? '' : 'now-product-type'}>票券商品(<span>2</span>)</li>
+          <li
+            onClick={() => {
+              setType(3)
+            }}>住宿商品(<span>0</span>)</li>
         </ul>
 
         {/* 2-2 購物車table */}
         <CardList />
       </div>
       {/* 3.按鈕列 */}
-      <div>
+      <div className='pb-4'>
         <button className='btn ' style={{ backgroundColor: '#7FB8B6', color: '#fff' }}>繼續購物</button>
         <button className='btn ' style={{ backgroundColor: '#D05B62', color: '#fff' }}>刪除全部商品</button>
         <button className='btn ' style={{ backgroundColor: '#FFc700', color: '#fff' }}>去買單</button>

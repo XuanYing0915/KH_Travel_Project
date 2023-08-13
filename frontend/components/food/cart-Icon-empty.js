b5bebf2c9fb3c235b55fe32019fbb96d7ec6e8ce
utlxj123 <132909849+utlxj123@users.noreply.github.com>
2023-08-13 22:03:31 +0800

    0813 (家) 加入購物車完成一半

--- frontend/components/food/cart-Icon-empty.js
+++ frontend/components/food/cart-Icon-empty.js
@@ -1,4 +1,4 @@
-export default function loveIcon() {
+export default function CartIconEmpty() {
     return (
         <>
             <svg
--- frontend/components/food/product-card.js
+++ frontend/components/food/product-card.js
@@ -1,112 +1,87 @@
-// 引入 React 的 useState 鉤子
-import React, { useState } from 'react'
+// 引入 React 所需的函數和元件
+import React, { useState, useEffect } from 'react'
+import CartIconEmpty from './cart-Icon-empty'
+import CartIconFilled from './cart-Icon-filled'
 
-// 引入 Next.js 的 Link 組件，用於處理客戶端路由跳轉
-import Link from 'next/link'
-// 引入自定義的 icon 組件
-import LoveIcon from './love-icon'
-import NoLoveIcon from './nolove-icon'
-import CartIcon from './crat-icon' // 注意: 這裡可能有拼寫錯誤，應該是'cart-icon'
-
-// 定義 ProductCard 組件，帶有預設參數
 export default function ProductCard({
+  // 設定產品卡片的預設屬性
   id = 1,
   img_src = 'images (2).jpg',
   name = '十分飽大拼盤',
-  time = '130 ',
+  price = '130 ',
   introduce = '品味與濃郁交織，香氣四溢，令人沉醉於其中的絕佳咖啡享受。',
-  like = false,
-  cart_src = '#',
-  towheresrc = '#',
-  status = 4,
 }) {
-  // 定義 img 變數，用於圖片的路徑
-  const img = `/images/food/${img_src}`
-  // 使用 useState 來存儲產品的喜歡狀態
-  const [lovestate, setLoves] = useState(like)
-  // 定義切換喜歡狀態的函數
-  const toggleFav = (clickid) => {
-    if (id === clickid) {
-      setLoves(!lovestate)
+  const img = `/images/food/${img_src}` // 設定圖片路徑
+  const [hover, setHover] = useState(false) // 設定滑鼠是否懸停的狀態
+  const [inCart, setInCart] = useState(false) // 設定產品是否在購物車裡的狀態
+
+  // 取得購物車中的商品
+  const getCartItems = () => {
+    const foodCart = localStorage.getItem('foodCart') || '[]'
+    const cartIds = JSON.parse(foodCart)
+    return cartIds.map((itemId) => JSON.parse(localStorage.getItem(itemId)))
+  }
+
+  // 使用 useEffect 來檢查商品是否已經在購物車裡
+  useEffect(() => {
+    const foodCart = JSON.parse(localStorage.getItem('foodCart') || '[]')
+    setInCart(foodCart.includes(id))
+  }, [id])
+
+  // 處理添加或移除購物車的邏輯
+  const addToCart = () => {
+    if (!inCart) {
+      // 若商品不在購物車裡
+      // 將商品加入購物車
+      const foodCart = localStorage.getItem('foodCart') || '[]'
+      const cartList = JSON.parse(foodCart)
+      cartList.push(id)
+      localStorage.setItem('foodCart', JSON.stringify(cartList))
+      localStorage.setItem(
+        id,
+        JSON.stringify({ id, name, img_src, price, countl: 1 })
+      )
+      alert('已加入購物車') // 顯示加入購物車的訊息
+      setInCart(true) // 更新狀態為已加入購物車
+    } else {
+      // 若商品已在購物車裡
+      // 將商品從購物車移除
+      const foodCart = localStorage.getItem('foodCart') || '[]'
+      const cartList = JSON.parse(foodCart).filter((itemId) => itemId !== id)
+      localStorage.setItem('foodCart', JSON.stringify(cartList))
+      localStorage.removeItem(id)
+      alert('已從購物車移除') // 顯示已從購物車移除的訊息
+      setInCart(false) // 更新狀態為已移除
     }
   }
 
-  // 使用 useState 存儲滑鼠懸停的狀態
-  const [hover, setHover] = useState(false)
-  // 定義改變滑鼠懸停狀態的函數
-  const hoverchange = (hoverstate) => {
-    setHover(hoverstate)
-  }
-
+  // 產品卡片的結構和樣式
   return (
     <>
       <div
-        className="commonCard2" // 定義 CSS class
+        className="commonCard2"
         key={id}
-        // 用於檢測滑鼠進入和離開的事件
-        onMouseEnter={() => {
-          hoverchange(true)
-        }}
-        onMouseLeave={() => {
-          hoverchange(false)
-        }}
+        onMouseEnter={() => setHover(true)} // 滑鼠進入時，設定懸停狀態
+        onMouseLeave={() => setHover(false)} // 滑鼠離開時，取消懸停狀態
       >
-        {/* 使用 Next.js 的 Link 組件進行路由跳轉 */}
-        <Link href={towheresrc} style={{ textDecoration: 'none' }}>
-          <div className={hover ? 'imgboxhover imgbox' : 'imgbox'}>
-            {/* 產品圖片 */}
-            <img
-              src={img}
-              style={{ height: '100%', width: '100%' }}
-              alt={name}
-            />
-          </div>
-          <div className="textbox">
-            {/* 產品名稱 */}
-            <h4 className={status > 1 ? 'font h4' : 'font h4 text-center'}>
-              {name}
-            </h4>
-            <div className="card2down">
-              <div>
-                {/* 產品時間和介紹，根據 status 來決定是否顯示 */}
-                {status > 1 ? (
-                  <p className="font p p-st2">{introduce}</p>
-                ) : (
-                  <p className="fontnull">1</p>
-                )}
-                {status > 2 ? <p className="font p p-st1">${time}</p> : ''}
-              </div>
-              <div className="iconblock">
-                {/* 根據 status 顯示喜愛或購物車圖標 */}
-                {status < 4 ? (
-                  <button
-                    className="buttonStyle"
-                    onClick={(e) => {
-                      e.preventDefault()
-                      toggleFav(id)
-                    }}
-                  >
-                    {lovestate ? <LoveIcon /> : <NoLoveIcon />}
-                  </button>
-                ) : (
-                  ''
-                )}
-                {status > 3 ? (
-                  <button
-                    className="buttonStyle"
-                    onClick={(e) => {
-                      e.preventDefault()
-                    }}
-                  >
-                    <CartIcon />
-                  </button>
-                ) : (
-                  ''
-                )}
-              </div>
+        <div className={hover ? 'imgboxhover imgbox' : 'imgbox'}>
+          <img src={img} style={{ height: '100%', width: '100%' }} alt={name} />
+        </div>
+        <div className="textbox">
+          <h4 className="font h4">{name}</h4>
+          <div className="card2down">
+            <div>
+              <p className="font p p-st2">{introduce}</p>
+              <p className="font p p-st1">${price}</p>
+            </div>
+            <div className="iconblock">
+              <button className="buttonStyle" onClick={addToCart}>
+                {/* 根據產品是否在購物車中，顯示不同的圖示 */}
+                {inCart ? <CartIconFilled /> : <CartIconEmpty />}
+              </button>
             </div>
           </div>
-        </Link>
+        </div>
       </div>
     </>
   )

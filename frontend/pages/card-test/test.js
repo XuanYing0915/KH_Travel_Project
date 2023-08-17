9ef32d4c3d8108293d3898450279a6467492cc39
qazwsxed1236987 <qazwsxed369@gmail.com>
2023-08-18 02:06:30 +0800

    測試狀態改變及抓取時間

---
+++ frontend/pages/card-test/test.js
@@ -0,0 +1,26 @@
+import Demon from '@/components/ticket/index-use/demon'
+import { useState, useEffect } from "react";
+
+
+export default function Counter() {
+
+
+
+
+
+
+
+
+
+
+    return (
+        <>
+            <Demon />
+        </>
+    );
+
+
+
+
+
+}

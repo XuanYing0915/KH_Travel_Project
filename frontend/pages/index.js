e44b50bc83bad820a349b3f9106dab41ebf3dd7d
Rynic <wendy9245@gmail.com>
2023-08-24 13:37:39 +0800

    首頁card

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,8 +7,46 @@ import HomepageCardSlider from '@/components/homepage/homepage-card1-slider'
 import Homecard2 from '@/components/homepage/homepage-card2'
 import 'aos/dist/aos.css'
 import 'animate.css'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
 
 export default function Home() {
+  const [card, setCard] = useState([])
+  const getRandomCards = (data, n) => {
+    const allCards = [...data] // 複製一份原始的資料
+    // 隨機排序
+    allCards.sort(() => Math.random() - 0.5)
+    const randomCards = allCards.slice(0, n)
+    setCard(randomCards)
+  }
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3005/attraction')
+      // 存入前端
+      // setAttractions(response.data)
+      // console.log('資料庫資料:', response.data)
+      getRandomCards(response.data, 15)
+
+    } catch (error) {
+      console.error('錯誤:', error)
+    }
+  }
+  const settings = {
+    dots: true,
+
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 5,
+    slidesToScroll: 3,
+
+  }
+  useEffect(() => {
+
+    fetchData()
+  }, [])
+  console.log(card)
   return (
     <div id="homepage">
       {/* 1. 輪播圖 */}
@@ -33,6 +71,71 @@ export default function Home() {
         <div className="slider-container">
           {/* 1-2-1輪播圖 */}
           <HomepageSlider />
+
+          {/* <Slider {...settings}>
+
+            <div className='slide-pic' >
+              <Homecard1
+                id={1}
+                img_src="美麗島.jpg"
+                name="旗津星空隧道"
+                like={false}
+                towheresrc="/attraction#600001"
+                imgrouter="attraction"
+              />
+            </div>
+            <div className='slide-pic'>
+              <Homecard1
+                id={2}
+                img_src="草神.jpg"
+                name="蓮池潭"
+                like={false}
+                towheresrc="/attraction#600002"
+                imgrouter="attraction"
+              />
+            </div>
+            <div className='slide-pic'>
+              <Homecard1
+                id={3}
+                img_src="高雄流行音樂中心.jpg"
+                name="草衙飛行器館"
+                like={false}
+                towheresrc="/attraction#600003"
+                imgrouter="attraction"
+              />
+            </div>
+            <div className='slide-pic'>
+              <Homecard1
+                id={4}
+                img_src="鍾離.jpg"
+                name="夢時代購物中心"
+                like={false}
+                towheresrc="/attraction#600004"
+                imgrouter="attraction"
+              />
+            </div>
+            <div className='slide-pic'>
+              <Homecard1
+                id={5}
+                img_src="流行音樂中心.png"
+                name="旗津海灘"
+                like={false}
+                towheresrc="/attraction#600005"
+                imgrouter="attraction"
+              />
+            </div>
+            <div className='slide-pic'>
+              <Homecard1
+                id={6}
+                img_src="草神.jpg"
+                name="草衙古道"
+                like={false}
+                towheresrc="/attraction#600006"
+                imgrouter="attraction"
+              />
+            </div>
+
+          </Slider> */}
           {/* 1-2-2輪播圖文字 */}
           <span className="homepage-text" id="homepage-text1">
             在高雄
@@ -84,7 +187,7 @@ export default function Home() {
         </div>
 
         {/* 2-2 輪播圖 */}
-        <HomepageCardSlider />
+        <HomepageCardSlider card={card} />
       </section>
 
       {/* 3.天氣API */}
